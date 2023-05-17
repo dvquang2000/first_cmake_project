1e7e25e63233833da90f942418f634770a607a43
dvquang2000 <quangdv.uet@gmail.com>
2023-05-17 10:42:22 +0700

    test

---
+++ build/Linux/aarch64/src/mylib1/CMakeFiles/mylib1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mylib1.dir/mylib1.cpp.o"
+  "libmylib1.a"
+  "libmylib1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mylib1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
