eb1430b0e23379786ddeed45df79644a442f552f
dvquang2000 <quangdv.uet@gmail.com>
2023-05-22 08:58:29 +0700

    run exe

--- run.sh
+++ run.sh
@@ -3,10 +3,11 @@ if [[ $OSTYPE == *"darwin"* ]]; then
     cd build/macOS/
     ./first_tests
 elif [[ $OSTYPE == *"linux-gnu"* ]]; then
-    cd build/Linux/aarch64/app
+    cd build/Linux/aarch64/
     # Disable running executable until Headless rendering is set up.
-    file execuatable
-    ./execuatable
+    file first_tests
+    ./first_tests
+    
 else
     echo "macOS ( 64-bit ) or Linux ( ARM-64 ) not detected. For windows platform, use run.bat file."
 fi
